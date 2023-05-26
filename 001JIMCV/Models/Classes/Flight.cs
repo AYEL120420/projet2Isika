eeb9bd3a4c1f98277127e408ac02045690a93d2d
antho30 <anthony.dumas30@gmail.com>
2023-05-26 15:18:00 +0200

    CImplémentation base de données vol + services + users

--- 001JIMCV/Models/Classes/Flight.cs
+++ 001JIMCV/Models/Classes/Flight.cs
@@ -2,12 +2,11 @@
 {
     public class Flight
     {
-        public int Id { get; set; }
+        public string FlightId { get; set; }
         public string Airline { get; set; }
-        public string FlightNumber { get; set; }
+        public float FlightNumber { get; set; }
         public string DepartureCity { get; set; }
         public string DestinationCity { get; set; }
         public string DepartureDate { get; set; }
-        public string ArrivalDate { get; set; }
     }
 }
