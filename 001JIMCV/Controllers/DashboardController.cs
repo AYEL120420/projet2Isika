28b53004ddd142a5655b24b743f3a2b5b2a3d84c
tstho <ts.salmon@proton.me>
2023-06-02 17:31:18 +0200

    modif lien burger

--- 001JIMCV/Controllers/DashboardController.cs
+++ 001JIMCV/Controllers/DashboardController.cs
@@ -1,22 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _001JIMCV.Models.Classes;
+using _001JIMCV.Models.Classes.Enum;
+using _001JIMCV.Models.Dals;
+using _001JIMCV.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using XAct.Users;
 
-namespace _001JIMCV.Controllers
+public class DashboardController : Controller
 {
-    public class DashboardController : Controller
+    private DashboardDal dashboardDal;
+    private LoginDal loginDal;
+    private AccommodationDal accommodationDal;
+    public DashboardController()
     {
-        public IActionResult Admin()
+        dashboardDal = new DashboardDal();
+        loginDal = new LoginDal();
+        accommodationDal = new AccommodationDal();
+    }
+
+    public ActionResult Index()
+    {
+        LoginViewModel viewModel = new LoginViewModel { Authentified = HttpContext.User.Identity.IsAuthenticated };
+        if (viewModel.Authentified)
         {
-            return View("AdminDashboard");
+            viewModel.User = loginDal.GetUser(HttpContext.User.Identity.Name);
+            UserEnum role = viewModel.User.Role; // Accéder au rôle de l'utilisateur
+
+            switch (role)
+            {
+                case UserEnum.Admin:
+                    // administrateur
+                    return View("AdminDashboard", viewModel);
+                case UserEnum.Customer:
+                    //  client
+                    return View("CustomerDashboard", viewModel);
+                case UserEnum.Provider:
+                    //  partenaire
+                    return View("ProviderDashboard", viewModel);
+                default:
+                    // Rôle non reconnu
+                    return View("Error");
+            }
         }
 
-        public IActionResult Provider()
-        {
-            return View("ProviderDashboard");
-        }
-
-        public IActionResult Client()
-        {
-            return View("ClientDashboard");
-        }
+        return View(viewModel);
     }
 }
