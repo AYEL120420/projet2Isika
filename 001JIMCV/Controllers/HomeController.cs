b97ca50d7dc4258f71d318c9867439d55833b7ce
tstho <ts.salmon@proton.me>
2023-06-05 08:58:25 +0200

    Duplique affichage voyage

--- 001JIMCV/Controllers/HomeController.cs
+++ 001JIMCV/Controllers/HomeController.cs
@@ -38,21 +38,29 @@ namespace _001JIMCV.Controllers
 
         public IActionResult ItalieFullPackage01()
         {
+            List<Journey> ItalieJourneys = JourneyDal.GetAllJourneys().Where(j => j.CountryDestination == "Italie").ToList();
+            ViewData["ItalieJourneys"] = ItalieJourneys;
             return View("Italie/ItalieFullPackage01");
         }
 
         public IActionResult JaponFullPackage01()
         {
+            List<Journey> JaponJourneys = JourneyDal.GetAllJourneys().Where(j => j.CountryDestination == "Japon").ToList();
+            ViewData["JaponJourneys"] = JaponJourneys;
             return View("Japon/JaponFullPackage01");
         }
 
         public IActionResult MexiqueFullPackage01()
         {
+            List<Journey> MexiqueJourneys = JourneyDal.GetAllJourneys().Where(j => j.CountryDestination == "Mexique").ToList();
+            ViewData["MexiqueJourneys"] = MexiqueJourneys;
             return View("Mexique/MexiqueFullPackage01");
         }
 
         public IActionResult VietnamFullPackage01()
         {
+            List<Journey> VietnamJourneys = JourneyDal.GetAllJourneys().Where(j => j.CountryDestination == "Vietnam").ToList();
+            ViewData["VietnamJourneys"] = VietnamJourneys;
             return View("Vietnam/VietnamFullPackage01");
         }
         public IActionResult MonProfil()
