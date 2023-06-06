3cfc811ecf667672039f5285639614ff11771922
tstho <ts.salmon@proton.me>
2023-06-06 15:49:31 +0200

    pause merge

--- 001JIMCV/Controllers/JourneyController.cs
+++ 001JIMCV/Controllers/JourneyController.cs
@@ -26,82 +26,89 @@ namespace _001JIMCV.Controllers
         {
             return View();
         }
+        public JourneyViewModel GetJourneyViewModelFull(int JourneyId)
+        {
+            JourneyViewModel jvm = new JourneyViewModel();
+
+            //On récupère le voyage associé à l'Id
+            Journey journey = JourneyDal.GetAllJourneys().Where(r => r.Id == JourneyId).FirstOrDefault();
+
+            if (journey != null)
+            {
+                jvm.Journey = journey;
+
+                // On récupère le Pack de Service associé à l'id du voyage et qui est clé en main
+                jvm.PackService = JourneyDal.GetAllPacks().Where(r => r.JourneyId == JourneyId & r.AllInclusive == true).FirstOrDefault();
+
+
+                //On récupère les vols associé au pack de Service
+                FlightPackServices flightPackServices = JourneyDal.GetFLightPackServices(jvm.PackService.Id);
+                jvm.DepartureFlight = JourneyDal.GetFlight(flightPackServices.DepartureFlightId);
+                jvm.ReturnFlight = JourneyDal.GetFlight(flightPackServices.ReturnFlightId);
+
+                // On récupère les Hébergements associés au Pack
+                List<AccommodationPackServices> accommodationsPackServices = JourneyDal.GetAllAccommodationsPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
+                List<Accommodation> accommodations = new List<Accommodation>();
+                foreach (AccommodationPackServices aps in accommodationsPackServices)
+                {
+                    Accommodation accommodation = new Accommodation();
+                    accommodation = JourneyDal.GetAccommodation(aps.AccommodationId);
+                    accommodations.Add(accommodation);
+                }
+                jvm.Accommodations = accommodations;
+
+                // On récupère les Activités associés au Pack
+                List<ActivityPackServices> activityPackServices = JourneyDal.GetAllActivityPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
+                List<Activity> activities = new List<Activity>();
+                foreach (ActivityPackServices aps in activityPackServices)
+                {
+                    Activity activity = new Activity();
+                    activity = JourneyDal.GetActivity(aps.ActivityId);
+                    activities.Add(activity);
+                }
+                jvm.Activities = activities;
+
+                // On récupère les restaurants associés au Pack
+                List<RestaurationPackServices> restaurationPackServices = JourneyDal.GetAllRestaurationPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
+                List<Restauration> restaurations = new List<Restauration>();
+                foreach (RestaurationPackServices aps in restaurationPackServices)
+                {
+                    Restauration restauration = new Restauration();
+                    restauration = JourneyDal.GetRestauration(aps.RestaurationId);
+                    restaurations.Add(restauration);
+                }
+                jvm.Restaurations = restaurations;
+            }
+            return jvm;
+        }
 
         public IActionResult DisplayJourney(int Id)
         {
             if (Id != 0)
             {
-                //On récupère le voyage associé à l'Id
-                Journey journey = JourneyDal.GetAllJourneys().Where(r => r.Id == Id).FirstOrDefault();
-
-                if (journey != null)
-                {
-                    // on crée le viewmodel du voyage
-                    JourneyViewModel jvm = new JourneyViewModel
-                    {
-                        Journey = journey,
-
-                        // On récupère le Pack de Service associé à l'id du voyage et qui est clé en main
-                        PackService = JourneyDal.GetAllPacks().Where(r => r.JourneyId == Id & r.AllInclusive == true).FirstOrDefault(),
-
-                    };
-
-                    //On récupère les vols associé au pack de Service
-                    FlightPackServices flightPackServices = JourneyDal.GetFLightPackServices(jvm.PackService.Id);
-                    jvm.DepartureFlight = JourneyDal.GetFlight(flightPackServices.DepartureFlightId);
-                    jvm.ReturnFlight = JourneyDal.GetFlight(flightPackServices.ReturnFlightId);
-
-                    // On récupère les Hébergements associés au Pack
-                    List<AccommodationPackServices> accommodationsPackServices = JourneyDal.GetAllAccommodationsPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
-                    List<Accommodation> accommodations = new List<Accommodation>();
-                    foreach (AccommodationPackServices aps in accommodationsPackServices)
-                    {
-                        Accommodation accommodation = new Accommodation();
-                        accommodation = JourneyDal.GetAccommodation(aps.AccommodationId);
-                        accommodations.Add(accommodation);
-                    }
-                    jvm.Accommodations = accommodations;
-
-                    // On récupère les Activités associés au Pack
-                    List<ActivityPackServices> activityPackServices = JourneyDal.GetAllActivityPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
-                    List<Activity> activities = new List<Activity>();
-                    foreach (ActivityPackServices aps in activityPackServices)
-                    {
-                        Activity activity = new Activity();
-                        activity = JourneyDal.GetActivity(aps.ActivityId);
-                        activities.Add(activity);
-                    }
-                    jvm.Activities = activities;
-
-                    // On récupère les restaurants associés au Pack
-                    List<RestaurationPackServices> restaurationPackServices = JourneyDal.GetAllRestaurationPackServices().Where(r => r.PackServicesId == jvm.PackService.Id).ToList();
-                    List<Restauration> restaurations = new List<Restauration>();
-                    foreach (RestaurationPackServices aps in restaurationPackServices)
-                    {
-                        Restauration restauration = new Restauration();
-                        restauration = JourneyDal.GetRestauration(aps.RestaurationId);
-                        restaurations.Add(restauration);
-                    }
-                    jvm.Restaurations = restaurations;
+                JourneyViewModel jvm = GetJourneyViewModelFull(Id);
 
 
-                    return View("Produits", jvm);
-                }
+                return View("Produits", jvm);
+
 
             }
 
             return View("Error");
         }
+
         [HttpPost]
-        public IActionResult ReservationAllInclusive(JourneyViewModel jvm)
+        public IActionResult ReservationAllInclusive(int Id)
         {
             LoginViewModel loginViewModel = new LoginViewModel { Authentified = HttpContext.User.Identity.IsAuthenticated };
             if (!HttpContext.User.Identity.IsAuthenticated)
             {
-                return Redirect("Login/Index");
+                return RedirectToAction("Index", "Login");
             }
             else
             {
+                JourneyViewModel jvm = GetJourneyViewModelFull(Id);
+
                 string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
                 loginViewModel.User = LoginDal.GetUser(userId);
 
@@ -141,14 +148,16 @@ namespace _001JIMCV.Controllers
 
             return View("Error");
         }
-        public IActionResult DisplayJourneyPerso(JourneyViewModel jvm, string departureCity)
+        public IActionResult DisplayJourneyPerso(int Id, string departureCity)
         {
             if (!HttpContext.User.Identity.IsAuthenticated)
             {
-                return Redirect("Login/Index");
+                return RedirectToAction("Index", "Login");
             }
             else
             {
+                JourneyViewModel jvm = GetJourneyViewModelFull(Id);
+
                 if (jvm.Journey != null)
                 {
                     ViewBag.listFlightsDep = JourneyDal.GetAllFLights().Where(f => f.DestinationCountry == jvm.Journey.CountryDestination & f.DepartureCity == departureCity);
@@ -157,7 +166,7 @@ namespace _001JIMCV.Controllers
                     ViewBag.listActivities = JourneyDal.GetAllActivities().Where(a => a.Country == jvm.Journey.CountryDestination);
                     ViewBag.listRestaurants = JourneyDal.GetAllRestaurations().Where(r => r.Country == jvm.Journey.CountryDestination);
 
-                    return View("ModifProduits",jvm);
+                    return View("ModifProduits", jvm);
                 }
 
             }
